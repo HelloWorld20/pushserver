92aea8eebcd9cdb2f5f5764bad6ebd1aa1612a9a
HelloWorld20 <292791515@qq.com>
2022-07-17 22:48:01 +0800

    'init'

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import Koa from "koa";
+import config from "../config";
+import Router from "@koa/router";
+// import Boom from "boom";
+
+import sendToWecom from "./send-to-wecom";
+
+const app = new Koa();
+
+const rootRouter = new Router();
+
+app.use(rootRouter.routes());
+
+app.use(async (ctx) => {
+  let text = ctx.request.query.text;
+  const cid = ctx.request.query.cid;
+  //   if (!text) throw Boom.badRequest("需要传递text参数");
+
+  if (Array.isArray(text)) {
+    text = text[0]
+  }
+
+  const result = await sendToWecom({
+    text: text ? text : "推送测试\r\n测试换行",
+    wecomAgentId: config.wecomAgentId,
+    wecomSecret: config.wecomSecret,
+    wecomCId: config.wecomCId + cid,
+  }).catch((err) => {
+    console.log(
+      "%c [ err ]-29",
+      "font-size:13px; background:pink; color:#bf2c9f;",
+      err
+    );
+    console.log(err);
+  });
+
+  ctx.body = JSON.stringify(result);
+});
+
+app.listen(config.port, () => {
+  console.log("server is running in " + config.port);
+});
---
+++ src/send-to-wecom.ts
@@ -0,0 +1,43 @@
+import request from "axios";
+// const config = require('../config');
+import config from '../config';
+
+export default async function sendToWecom(body: {
+  text: string;
+  wecomCId: string;
+  wecomSecret: string;
+  wecomAgentId: string;
+  wecomTouid?: string;
+}): Promise<{ errcode: number; errmsg: string; invaliduser: string }> {
+  body.wecomTouid = body.wecomTouid ? "@all" : '';
+  const getTokenUrl = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${body.wecomCId}&corpsecret=${body.wecomSecret}`;
+  const getTokenRes = await request.get(getTokenUrl);
+  const accessToken = getTokenRes.data.access_token;
+  if (accessToken && accessToken.length <= 0) {
+    throw new Error("获取 accessToken 失败");
+  }
+  const sendMsgUrl = `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${accessToken}`;
+  const sendMsgRes = await request.post(sendMsgUrl, {
+    touser: body.wecomTouid,
+    agentid: body.wecomAgentId,
+    msgtype: "text",
+    text: {
+      content: body.text,
+    },
+    duplicate_check_interval: 600,
+  });
+  return sendMsgRes.data;
+}
+
+// sendToWecom({
+//   text: '推送测试\r\n测试换行',
+//   wecomAgentId: config.wecomAgentId,
+//   wecomSecret: config.wecomSecret,
+//   wecomCId: config.wecomCId,
+// })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
